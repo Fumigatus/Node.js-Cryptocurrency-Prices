638c6e47c54d061773fbe1627c755473666d4cf8
Fumigatus <alpcan237@gmail.com>
2022-05-21 16:07:05 +0300

    set key function and validation

--- commands/key.js
+++ commands/key.js
@@ -1,12 +1,28 @@
+const inquirer = require('inquirer')
+const KeyManager = require('../lib/KeyManager.js')
+const { isRequired } = require('../utils/validation')
+
 const key = {
-    set() {
-        console.log('TODO: set key value')
+    async set() {
+        const keyManager = new KeyManager()
+        const input = await inquirer.prompt({
+            type: 'input',
+            name: 'key',
+            message: 'Enter API key',
+            validate: isRequired
+        })
+        const key = keyManager.setKey(input.key)
+
+        if (key) {
+            console.log('API key set')
+        }
+
     },
     show() {
         console.log('TODO: show key value')
     },
     delete() {
-        console.log('TODO: delete key value')
+
     }
 }
 
